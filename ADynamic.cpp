36063dd4d1b0f9645b558e06ba5a773773ac3c4b
serimezarci <60902058+zeynpengnn@users.noreply.github.com>
2021-06-01 16:57:31 +0300

    Add files via upload

---
+++ ADynamic.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main() { 
+      int a,b,c,d;
+
+
+
+  
+    a = 48;
+    while (a <= 57)
+    {
+        printf("%d = %c\n", a, a);
+        a++;
+    }
+     
+for(b=65;b<=90;b++)
+  {
+	  int b;
+    b = 65;
+    while (b <= 90)
+    {
+        printf("%d = %c\n", b, b);
+        b++;
+    }}
+for(c=65;c<=90;c++)
+{
+
+    c= 65;
+    while (c <= 90)
+    {
+        printf("%d = %c\n", c, c);
+        c++;
+    }
+    return 0;
+} }
---
+++ MP.cpp
@@ -0,0 +1,155 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
+int main()
+{   
+  char password[4];
+ 
+ 
+  printf("enter a password:/n");
+  
+
+for (int i= 0; i<= 4; i++)
+    {
+		password[i] = getch ();
+      
+		putchar ('*');
+    
+	} 
+	if(strcmp(password,"1234")==0)
+	
+        printf("\n Giris Basarili");
+        
+    else printf("\n Basarisiz");
+
+  
+  
+for (int password[0]=0; password[0]<=count; password[0]++)
+
+if (ch<='9'  || ch>='0')
+	{				
+	  password[0]=a;
+	  count++;
+	  scanf("%c",&a)
+	  } 
+
+if (ch<='A'  || ch>='Z')
+	{  
+	 password[0]=c;
+	 count++;
+	 scanf("%c",&a)
+}
+
+else 
+
+if(ch>="a" || ch<="z") 
+
+{  	    password[0]=d;
+		count++;
+		printf("%d")
+
+
+}   
+for (int password[1]=0;password[1]<=count;password++)
+
+if (ch<='9'  || ch>='0')
+	{				
+	  password[1]=a;
+	  count++;
+	  scanf("%c",&a)
+	  } 
+
+if (ch<='A'  || ch>='Z')
+	{  
+	 password[1]=b;
+	 count++
+	 scanf("%c",&b)
+}
+
+else 
+
+if(ch>="a" || ch<="z") 
+
+{  	    password[1]=d;
+		count++;
+		printf("%d",d)
+
+
+}   
+
+for (int password[2]=0; password[2]<=count; password[2]++)
+
+
+if (ch<='9'  || ch>='0')
+	{				
+	  password[2]=a;
+	  count++;
+	  scanf("%c",&a)
+	  } 
+
+if (ch<='A'  || ch>='Z')
+	{  
+	 password[2]=c;
+	 count++;
+	 scanf("%c",&c)
+}
+
+else 
+
+if(ch>="a" || ch<="z") 
+
+{  	    password[2]=d;
+		count++;
+		printf("%d",d)
+
+
+}   
+
+for (int password[3]=0; password[3]<=count; password[3]++)
+
+
+if (ch<='9'  || ch>='0')
+	{				
+	  password[3]=a;
+	  count++;
+	  scanf("%c",&a)
+	  } 
+
+if (ch<='A'  || ch>='Z')
+	{  
+	 password[3]=c;
+	 count++;
+	 scanf("%c",&c)
+}
+
+else 
+
+if(ch>="a" || ch<="z") 
+
+{  	    password[3]=d;
+		count++;
+		printf("%d",d)
+
+
+}   
+
+return 0;
+}
+
+
+
+
+
+
+
+ 
+   
+    
+	
+	
+	
+	
+	
+
